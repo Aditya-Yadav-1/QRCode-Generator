149e0a8603b33dfca7d378770ceec563415bbc5b
Aditya-Yadav-1 <97456323+Aditya-Yadav-1@users.noreply.github.com>
2023-07-09 22:42:07 +0530

    Update script.js

--- script.js
+++ script.js
@@ -8,18 +8,22 @@ const FGColor = document.getElementById("FGColor");
 let QR_Code;
 let sizeChoice, BGColorChoice, FGColorChoice;
 
+//Set size
 sizeOptions.addEventListener("click", () => {
   sizeChoice = sizeOptions.value;
 });
 
+//Set background color
 BGColor.addEventListener("input", () => {
   BGColorChoice = BGColor.value;
 });
 
+//Set foreground color
 FGColor.addEventListener("input", () => {
   FGColorChoice = FGColor.value;
 });
 
+//Format input
 const inputFormatter = (value) => {
   value = value.replace(/[^a-z0-9A-Z]+/g, "");
   return value;
@@ -27,7 +31,7 @@ const inputFormatter = (value) => {
 
 submitBtn.addEventListener("click", async () => {
   container.innerHTML = "";
-  
+  //QR code genertion
   QR_Code = await new QRCode(container, {
     text: userInput.value,
     width: sizeChoice,
@@ -36,6 +40,7 @@ submitBtn.addEventListener("click", async () => {
     colorLight: BGColorChoice,
   });
 
+  //Set url for download
   const src = container.firstChild.toDataURL("image/pmg");
   downloadBtn.href = src;
   let userValue = userInput.value;
@@ -68,3 +73,4 @@ window.onload = () => {
   downloadBtn.classList.add("hide");
   submitBtn.disabled = true;
 };
+
